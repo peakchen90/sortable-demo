403cca2619ddb54e453b7ca89e3a5d9ea831db99
peakchen90 <1558267774@qq.com>
2019-11-05 23:18:28 +0800

    +

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/sortable-demo/'
+    : '/',
   outputDir: 'dist',
   chainWebpack: (config) => {
     config.resolve.extensions.add('.vue');
